2b0d4ab3c19fc94c798b3f4d506fa646693a6f0f
lutri <lutringerkevin@gmail.com>
2024-12-18 14:41:56 +0100

    ajout des sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Carrousel from './components/carousel';
+import photo from './photo/photoPresentation.jpg';
+import Competences from './components/competencesTest'
+import './App.css'; // Import du fichier SCSS
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header Section */}
+      <Header />
+      
+      {/* Présentation Section */}
+      <section id="presentation" className="presentation">
+        <h2>Présentation</h2>
+        <div className="presentation-container">
+          <div className="image">
+            <img src={photo} alt="Mon portrait" />
+          </div>
+          <div className="text">
+            <p>
+              Passionné par la lecture, les jeux vidéo et l'informatique, je suis en pleine reconversion professionnelle dans le développement web.
+              Curieux et avide d’apprendre, je me suis plongé dans cet univers avec enthousiasme, motivé par l’envie de créer des solutions innovantes et intuitives.
+              Grâce à ma formation de developpeur web chez OpenClassroom, j’ai pu développer une approche créative et structurée des projets, tout en cultivant ma capacité à m’adapter rapidement à mon environnement.
+              Que ce soit en équipe ou en solo, j’aime relever de nouveaux défis et approfondir mes connaissances.
+              Mon objectif ? Apporter ma créativité et mon savoir-faire technique pour contribuer à des projets captivants, tout en continuant à évoluer dans un domaine en constante évolution.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Projets Section */}
+      <section id="projets" className="projets">
+        <h2>Mes projets</h2>
+        <Carrousel />
+      </section>
+
+      {/* Compétences Section */}
+      <section id="competences" className='competences'>
+        <Competences />
+      </section>
+      
+
+      {/* Footer Section */}
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
